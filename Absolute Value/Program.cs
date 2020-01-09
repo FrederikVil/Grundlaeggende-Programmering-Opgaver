eaade806df25b644fccf75018520af86ce64892b
Frederik Vilhelmsen <frederik@rovi.dk>
2020-01-09 09:32:02 +0100

    xd

---
+++ Absolute Value/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Absolute_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Finder den absolutte value af et tal.
+
+            int tal1, sum;
+
+            Console.Write("Skriv et tal: ");
+            tal1 = Convert.ToInt32(Console.ReadLine());
+
+            // Math.Abs laver et tal absolut
+            sum = Math.Abs(tal1);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
